1f81ed7539cb60dabb09fdc2ec46ed9d77a11cfb
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-11 00:40:42 +0700

    3 files modified, 1 added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
+import NutUI from '@nutui/nutui-jdl';
+import '@nutui/nutui-jdl/dist/nutui.css';
 
 createApp(App).mount('#app')
